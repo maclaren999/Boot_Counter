c6a7889e13f53797757d5168a93174d2587b5eef
artem.che <artem.chebotarenko@lyfo.com>
2024-10-16 18:51:07 +0300

    feat: Add Broadcast receiver

---
+++ app/src/main/java/com/bytebuddies/bootcounter/data/broadcast/BootReceiver.kt
@@ -0,0 +1,45 @@
+package com.bytebuddies.bootcounter.data.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.room.Room
+import com.bytebuddies.bootcounter.model.BootEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import java.util.Date
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.ExistingPeriodicWorkPolicy
+import com.bytebuddies.bootcounter.data.database.BootDatabase
+import com.bytebuddies.bootcounter.data.worker.NotificationWorker
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val db = Room.databaseBuilder(
+                context.applicationContext,
+                BootDatabase::class.java, "boot_counter_database"
+            ).build()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                db.bootEventDao().insert(BootEvent(timestamp = Date().time))
+            }
+
+            val workRequest = PeriodicWorkRequestBuilder<NotificationWorker>(15, TimeUnit.MINUTES)
+                .setInitialDelay(15, TimeUnit.MINUTES)
+                .build()
+
+            WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+                "BootCounterNotification",
+                ExistingPeriodicWorkPolicy.REPLACE,
+                workRequest
+            )
+
+            // Optional: Check if a notification was present before reboot and restore it
+            // This part can be implemented based on specific requirements
+        }
+    }
+}
