6391293c78aeb9b6a4e39cebdcb76d683c529646
artem.che <artem.chebotarenko@lyfo.com>
2024-10-16 19:00:04 +0300

    feat: Update MainActivity UI

--- app/src/main/java/com/bytebuddies/bootcounter/MainActivity.kt
+++ app/src/main/java/com/bytebuddies/bootcounter/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.bytebuddies.bootcounter
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bytebuddies.bootcounter.data.database.BootDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +23,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val bootEventsTextView: TextView = findViewById(R.id.bootEventsTextView)
+        val db = BootDatabase.getDatabase(applicationContext)
+        val bootEventDao = db.bootEventDao()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val bootEvents = bootEventDao.getAllBootEvents()
+            val bootEventsText = if (bootEvents.isEmpty()) {
+                "No boots detected"
+            } else {
+                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                bootEvents.groupBy { dateFormat.format(it.timestamp) }
+                    .map { (date, events) -> "$date - ${events.size}" }
+                    .joinToString("\n")
+            }
+
+            withContext(Dispatchers.Main) {
+                bootEventsTextView.text = bootEventsText
+            }
         }
     }
 }
